2be241394c220e0303684d8971951f016e5ce01e
RasmusYA <92869131+RasmusYA@users.noreply.github.com>
2022-03-23 21:33:06 +0100

    Update HuvudMenyController.java
    Code for switching scenes

--- src/main/java/com/grupp2/hardly/HuvudMenyController.java
+++ src/main/java/com/grupp2/hardly/HuvudMenyController.java
@@ -12,14 +12,28 @@ public class HuvudMenyController {
     @FXML
     private Button Val7;
     
-    @FXML
-    public void openNewCustomerWindow() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("/edu/petersson/wigellsconcert/NewCustomerWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 329, 245);
-        Stage stage = new Stage();
-        stage.setTitle("New Customer");
+    
+    public void val5(){
+        openScene2(5);
+    }
+    
+    public void val6(){
+        openScene2(6);
+    }
+    
+    public void val7(){
+        openScene2(7);
+    }
+    
+    public void openScene2(int wordLength) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("scene2Controller.fxml"));
+        Parent root = loader.load();
+        scene2Controller crmController = loader.getController();
+        //crmController.setWordLength(wordLength); // Sätter längden på ord i scene2 (setWordLength metoden är inte implementerad i scen2 än).
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
+        stage.centerOnScreen();
         stage.show();
     }
     

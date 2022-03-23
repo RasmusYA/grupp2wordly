43f691ce9830156f74bad5da3227ae94b1817fa5
sparre1995@gmail.com <Omg13374ever>
2022-03-23 16:00:45 +0100

    HUVUDMENY FIX

--- src/main/java/com/grupp2/hardly/HuvudMenyController.java
+++ src/main/java/com/grupp2/hardly/HuvudMenyController.java
@@ -3,7 +3,7 @@ package com.grupp2.hardly;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class HuvudMenyController {
     @FXML
     private Label welcomeText;
 

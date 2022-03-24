3ef83491defa563ac9654f6279ba1902ace542df
Simon Ulander <devsimonulander@gmail.com>
2022-03-24 12:47:27 +0100

    Added StringChecker

---
+++ src/main/java/com/grupp2/hardly/StringChecker.java
@@ -0,0 +1,32 @@
+package com.grupp2.hardly;
+
+public class StringChecker {
+    int[] Check(int len, String userString, String correctString) {
+        int[] result = new int[len];
+        /**
+         * 1 - correct
+         * 2 - exists in wrong position
+         * 3 - incorrect
+
+         */
+        for (int i = 0; i < len; i++) {
+            if (userString.charAt(i)==correctString.charAt(i)){
+                result[i] = 1;
+            }
+            if (userString.charAt(i)!=correctString.charAt(i)){
+                for(int k =0; k< len; k++ ){
+                    if (userString.charAt(i)!=correctString.charAt(k)){
+                        result[i] = 2;
+                    }
+
+                }
+            }
+            if (result[i] != 1 && result[i] != 2){
+                result[i] = 3;
+            }
+        }
+
+
+        return result;
+    }
+}

6a9274826e39bcc9b17703852d5cce63a8e75deb
Simon Ulander <devsimonulander@gmail.com>
2022-03-24 10:51:36 +0100

    Fixed some things.

--- src/main/java/com/grupp2/hardly/GameBoardController.java
+++ src/main/java/com/grupp2/hardly/GameBoardController.java
@@ -1,22 +1,32 @@
+package com.grupp2.hardly;
+
+import javafx.fxml.Initializable;
+
+import javax.persistence.*;
+import java.net.URL;
+import java.util.List;
+import java.util.Random;
+import java.util.ResourceBundle;
 
 public class GameBoardController implements Initializable {
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("hibernate");
-    private string theWord;
+    private String theWord;
 
 
     public void getRandomWord(){
 
         EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
         EntityTransaction transaction = null;
-        customerTableView.getItems().clear();
-        try {
+
+            try {
             transaction = em.getTransaction();
             transaction.begin();
 
             Random random = new Random();
             int randomNumber = random.nextInt(4);
-            List<Customer> customerList = em.createNativeQuery("SELECT id FROM fiveletter", FiveLetter.class).getResultList();
-
+            Query query = (Query) em.createNativeQuery("SELECT word FROM fiveletter where id = ?", FiveLetters.class).getResultList();
+            query.setParameter(1, randomNumber);
+            theWord = (String) query.getSingleResult();
             transaction.commit();
         } catch (Exception ex) {
             if (transaction != null)
